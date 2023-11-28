cd2c5290025108539f57d61766fcf7d6c393e7fd
Ezcoding96 <arkapbanerjee.96@gmail.com>
2023-11-28 12:12:35 -0800

    Added new comments to help under the code better

--- optimal_min_string.py
+++ optimal_min_string.py
@@ -5,7 +5,7 @@ for i in s:
     stack.append(i)
     print(stack)
     if len(stack)>1:
-        if "".join(stack[-2:]) in ["AB","CD"]:
+        if "".join(stack[-2:]) in ["AB","CD"]: #joins the last two entered elements in the list (stack) 
             stack.pop()
             stack.pop()
 print(len(stack))
