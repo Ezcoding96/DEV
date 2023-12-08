e65e8ce5c351f55d87cb6a1180dd07637997480a
Ezcoding96 <arkapbanerjee.96@gmail.com>
2023-12-08 05:49:43 -0800

    "Palindrom class to check for a palindrom string"

---
+++ palindrom.py
@@ -0,0 +1,14 @@
+class palindrom:
+    def __init__(self, a:str):
+        self.a=a
+    
+    def show(self):
+        print("String is: ",self.a)
+
+    def checkPalindrom(self):
+        temp_string=self.a.lower()
+        reverse_string="".join(sorted(temp_string,reverse=True))
+        if temp_string==reverse_string:
+            return True
+        else:
+            return False
